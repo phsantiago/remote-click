3669adb81c6104dace9e79347b27481ec635e08a
Pedro H. Santiago <pedroh_s_@hotmail.com>
2017-09-17 14:53:53 -0300

    first commit

---
+++ src/App.js
@@ -0,0 +1,21 @@
+import React, { Component } from 'react';
+import axios from 'axios'
+import FontAwesome from 'react-fontawesome';
+import './App.css';
+
+class App extends Component {
+  performClick(){
+    axios.get('http://localhost/click')
+  }
+  render() {
+    return (
+      <div className="App">
+         <div onClick={()=> this.performClick()} className="send">
+            <FontAwesome name='paper-plane' size="150px" />
+         </div>
+      </div>
+    );
+  }
+}
+
+export default App;
